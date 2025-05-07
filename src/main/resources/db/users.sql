bbd9a6629a80127786dc6a5fa4bb638b5fe7d30d
Espartaco Nassekine Local <espartaconassekine@gmail.com>
2025-05-07 16:01:07 +0200

    fix nuevo campo, Si se toca el modelo, agregar en las querys

--- src/main/resources/db/users.sql
+++ src/main/resources/db/users.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `enabled` tinyint(1) NOT NULL,
+  `fotoUrl` varchar(255),
   `organizacion_id` bigint(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_organizacion` (`organizacion_id`),
