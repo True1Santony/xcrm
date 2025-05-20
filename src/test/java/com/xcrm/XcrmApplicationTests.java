5f884844bf3cb0b66e51cf35bd7079ec7cd3af8c
True1Santony <espartaconassekine@gmail.com>
2025-05-20 21:28:32 +0200

    ec2 docs

--- src/test/java/com/xcrm/XcrmApplicationTests.java
+++ src/test/java/com/xcrm/XcrmApplicationTests.java
@@ -34,7 +34,7 @@ public class XcrmApplicationTests {
 
 	@Test
 	public void testConcurrentAccess() throws InterruptedException {
-		int threadCount = 2000;
+		int threadCount = 50;
 		CountDownLatch latch = new CountDownLatch(threadCount);
 		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
 
